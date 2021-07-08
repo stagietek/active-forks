afe5b5795008b44434772a40b14a948ccef1db27
Rıdvan Altun <ridvanaltun@outlook.com>
2021-07-08 15:28:08 +0300

    style: fix code style

--- src/main.js
+++ src/main.js
@@ -10,8 +10,7 @@ window.addEventListener('load', () => {
 
   try {
     const token = localStorage.getItem('token');
-    if (token)
-      document.getElementById('token').value = token;
+    if (token) document.getElementById('token').value = token;
   } catch {}
 
   if (repo) {
@@ -20,13 +19,13 @@ window.addEventListener('load', () => {
   }
 });
 
-document.getElementById('form').addEventListener('submit', e => {
+document.getElementById('form').addEventListener('submit', (e) => {
   e.preventDefault();
   fetchData();
 });
 
 function addDarkmodeWidget() {
-  new Darkmode( { label: '🌓' } ).showWidget();
+  new Darkmode({ label: '🌓' }).showWidget();
 }
 
 let running = false;
@@ -38,7 +37,7 @@ function fetchData() {
   }
   Runner.start();
 
-  hideMsg()
+  hideMsg();
 
   const repo = document.getElementById('q').value;
   const re = /[-_\w]+\/[-_.\w]+/;
@@ -75,13 +74,10 @@ function updateDT(data) {
     fork.name = `<a href="https://github.com/${fork.full_name}">${fork.name}</a>`;
     forks.push(fork);
   }
-  const dataSet = forks.map(fork =>
-    window.columnNamesMap.map(colNM => fork[colNM[1]])
+  const dataSet = forks.map((fork) =>
+    window.columnNamesMap.map((colNM) => fork[colNM[1]])
   );
-  window.forkTable
-    .clear()
-    .rows.add(dataSet)
-    .draw();
+  window.forkTable.clear().rows.add(dataSet).draw();
 }
 
 function initDT() {
@@ -103,33 +99,28 @@ function initDT() {
   // Sort by stars:
   const sortColName = 'Stars';
   const sortColumnIdx = window.columnNamesMap
-    .map(pair => pair[0])
+    .map((pair) => pair[0])
     .indexOf(sortColName);
 
   // Use first index for readable column name
   // we use moment's fromNow() if we are rendering for `pushed_at`; better solution welcome
   window.forkTable = $('#forkTable').DataTable({
-    columns: window.columnNamesMap.map(colNM => {
+    columns: window.columnNamesMap.map((colNM) => {
       return {
         title: colNM[0],
         render: (data, type, _row) => {
           switch (colNM[1]) {
-
             case 'pushed_at':
-              return type === 'display'
-                ? moment(data).fromNow()
-                : data;
+              return type === 'display' ? moment(data).fromNow() : data;
 
             case 'diff_from_original':
             case 'diff_to_original':
-              return type === 'display'
-                ? data
-                : data.substr(4, 4);
+              return type === 'display' ? data : data.substr(4, 4);
 
             default:
               return data;
           }
-        }
+        },
       };
     }),
     columnDefs: [
@@ -138,11 +129,10 @@ function initDT() {
       { width: '120px', targets: 7 }, // date
     ],
     order: [[sortColumnIdx, 'desc']],
-    createdRow: function(row, _, index) {
+    createdRow: function (row, _, index) {
       $('[data-toggle=popover]', row).popover();
-      if (index === 0)
-        row.classList.add('original-repo');
-    }
+      if (index === 0) row.classList.add('original-repo');
+    },
   });
 }
 
@@ -159,7 +149,7 @@ async function fetchAndShow(repo) {
   try {
     const maxRecords = Options.getAndSave().maxRecords;
 
-    const singleLimiter = fork => ({
+    const singleLimiter = (fork) => ({
       full_name: fork.full_name,
       name: fork.name,
       default_branch: fork.default_branch,
@@ -169,13 +159,16 @@ async function fetchAndShow(repo) {
       size: fork.size,
       pushed_at: fork.pushed_at,
       owner: {
-        login: fork.owner.login
-      }
+        login: fork.owner.login,
+      },
     });
 
-    const multiLimiter = data => data.map(singleLimiter);
+    const multiLimiter = (data) => data.map(singleLimiter);
 
-    const originalRepo = await api.fetch(`https://api.github.com/repos/${repo}`, singleLimiter);
+    const originalRepo = await api.fetch(
+      `https://api.github.com/repos/${repo}`,
+      singleLimiter
+    );
     originalRepo.diff_from_original = originalRepo.diff_to_original = '0';
     const originalBranch = originalRepo.default_branch;
     data.push(originalRepo);
@@ -203,7 +196,7 @@ async function fetchAndShow(repo) {
 }
 
 function hideMsg() {
-  document.getElementById('data-body').style.display= 'none';
+  document.getElementById('data-body').style.display = 'none';
 }
 
 function showMsg(msg, type) {
@@ -215,7 +208,7 @@ function showMsg(msg, type) {
 
   document.getElementById('footer').innerHTML = '';
 
-  document.getElementById('data-body').style.display= 'block';
+  document.getElementById('data-body').style.display = 'block';
   document.getElementById('data-body').innerHTML = `
         <div class="alert ${alert_type} alert-dismissible fade show" role="alert">
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -233,8 +226,9 @@ function getRepoFromUrl() {
 }
 
 async function updateData(repo, originalBranch, forks, api) {
-
-  forks.forEach(fork => fork.diff_from_original = fork.diff_to_original = '');
+  forks.forEach(
+    (fork) => (fork.diff_from_original = fork.diff_to_original = '')
+  );
 
   let index = 1;
   const quota = Quota(api);
@@ -269,7 +263,7 @@ async function updateData(repo, originalBranch, forks, api) {
 async function fetchMore(repo, originalBranch, fork, api) {
   return Promise.all([
     fetchMoreDir(repo, originalBranch, fork, true, api),
-    fetchMoreDir(repo, originalBranch, fork, false, api)
+    fetchMoreDir(repo, originalBranch, fork, false, api),
   ]);
 }
 
@@ -278,44 +272,47 @@ async function fetchMoreDir(repo, originalBranch, fork, fromOriginal, api) {
     ? `https://api.github.com/repos/${repo}/compare/${fork.owner.login}:${fork.default_branch}...${originalBranch}`
     : `https://api.github.com/repos/${repo}/compare/${originalBranch}...${fork.owner.login}:${fork.default_branch}`;
 
-  const limiter = data => ({
-    commits: data.commits.map(c => ({
+  const limiter = (data) => ({
+    commits: data.commits.map((c) => ({
       sha: c.sha.substr(0, 6),
       commit: {
         author: {
-          date: c.commit.author.date
+          date: c.commit.author.date,
         },
-        message: c.commit.message
+        message: c.commit.message,
       },
       author: {
-        login: c.author ? c.author.login : undefined
-      }
-    }))
+        login: c.author ? c.author.login : undefined,
+      },
+    })),
   });
   const data = await api.fetch(url, limiter);
 
-  if (fromOriginal)
-    fork.diff_from_original = printInfo('-', data, fork);
-  else
-    fork.diff_to_original = printInfo('+', data, fork);
+  if (fromOriginal) fork.diff_from_original = printInfo('-', data, fork);
+  else fork.diff_to_original = printInfo('+', data, fork);
 }
 
 function printInfo(sep, data, fork) {
   const length = data.commits.length;
-  if (length === 0)
-    return '0';
+  if (length === 0) return '0';
 
-  const details = '<pre>' +
+  const details =
+    '<pre>' +
     data.commits
-      .map(c => {
+      .map((c) => {
         c.author_date = c.commit.author.date.replace('Z', '').replace('T', ' ');
         c.author_login = c.author && c.author.login ? c.author.login : '-';
         const sha = c.sha.substr(0, 6);
-        c.link = `<a href="https://github.com/${fork.owner.login}/${fork.name}/commit/${sha}">${sha}</a>`
+        c.link = `<a href="https://github.com/${fork.owner.login}/${fork.name}/commit/${sha}">${sha}</a>`;
         return c;
-       })
-      .map(c => `${c.link} ${c.author_date.substr(0, 10)} ${c.author_login} - ${c.commit.message}`)
-      .map(s => s.replace(/[\n\r]/g, ' ').substr(0, 150))
+      })
+      .map(
+        (c) =>
+          `${c.link} ${c.author_date.substr(0, 10)} ${c.author_login} - ${
+            c.commit.message
+          }`
+      )
+      .map((s) => s.replace(/[\n\r]/g, ' ').substr(0, 150))
       .join('\n')
       .replace(/&/g, '&amp;')
       .replace(/"/g, '&quot;')
@@ -332,9 +329,13 @@ function Progress(max) {
   const $progress = $('.progress');
   const $bar = $('.progress-bar');
 
-  function show() { $progress.show(); }
+  function show() {
+    $progress.show();
+  }
 
-  function hide() { $progress.hide(); }
+  function hide() {
+    $progress.hide();
+  }
 
   function update(count) {
     const val = Math.round((count / max) * 100) + '%';
@@ -351,7 +352,9 @@ function Quota(api) {
   function update() {
     const rate = api.getLimits();
     const reset = moment(rate.reset).fromNow();
-    $quota.html(`Quota: left ${rate.remaining} / ${rate.limit}<br/>Reset ${reset}`);
+    $quota.html(
+      `Quota: left ${rate.remaining} / ${rate.limit}<br/>Reset ${reset}`
+    );
   }
 
   return { update };
@@ -360,16 +363,16 @@ function Quota(api) {
 function Api(token) {
   const config = token
     ? {
-      headers: {
-        authorization: "token " + token
+        headers: {
+          authorization: 'token ' + token,
+        },
       }
-    }
     : undefined;
 
   const rate = {
     remaining: '?',
     limit: '?',
-    reset: new Date()
+    reset: new Date(),
   };
 
   const cache = ApiCache();
@@ -378,10 +381,8 @@ function Api(token) {
     try {
       const { cached, newConfig } = cache.get(url, config);
       const response = await fetch(url, newConfig);
-      if (response.status === 304)
-        return cached.data;
-      if (!response.ok)
-        throw Error(response.statusText);
+      if (response.status === 304) return cached.data;
+      if (!response.ok) throw Error(response.statusText);
 
       updateRate(response);
 
@@ -391,7 +392,6 @@ function Api(token) {
       cache.add(url, limitedData, response);
 
       return limitedData;
-
     } catch (error) {
       const msg =
         error.toString().indexOf('Forbidden') >= 0
@@ -403,26 +403,28 @@ function Api(token) {
     }
   }
 
-  function getLimits() { return rate; }
+  function getLimits() {
+    return rate;
+  }
 
   async function refreshLimits() {
     const url = 'https://api.github.com/rate_limit';
     const response = await fetch(url, config);
-    if (response.ok)
-      updateRate(response);
+    if (response.ok) updateRate(response);
   }
 
   function updateRate(response) {
     rate.limit = response.headers.get('x-ratelimit-limit');
     rate.remaining = response.headers.get('x-ratelimit-remaining');
-    rate.reset = new Date(1000 * parseInt(response.headers.get('x-ratelimit-reset')));
+    rate.reset = new Date(
+      1000 * parseInt(response.headers.get('x-ratelimit-reset'))
+    );
   }
 
   return { fetch: get, getLimits, refreshLimits };
 }
 
 function ApiCache() {
-
   const map = new Map();
   const STORAGE = sessionStorage;
 
@@ -434,8 +436,7 @@ function ApiCache() {
     try {
       if (!cachedString) {
         cachedString = STORAGE.getItem(key);
-        if (cachedString)
-          map.set(key, cachedString);
+        if (cachedString) map.set(key, cachedString);
       }
     } catch {}
 
@@ -453,43 +454,48 @@ function ApiCache() {
     const val = JSON.stringify({
       etag: response.headers.get('etag'),
       date: new Date(),
-      data: limitedData
+      data: limitedData,
     });
 
     map.set(key, val);
     try {
       STORAGE.setItem(key, val);
-    } catch (err) {
-    }
+    } catch (err) {}
   }
 
   return { get, add };
 }
 
 const Runner = {
-  start: function() {
+  start: function () {
     running = true;
     $('#find .find-label').text('Stop');
     $('#find #spinner').addClass('d-inline-block');
   },
-  stop: function() {
+  stop: function () {
     running = false;
     $('#find .find-label').text('Find');
     $('#find #spinner').removeClass('d-inline-block');
-  }
+  },
 };
 
 const Options = {
-
-  loadAndShow: function() {
+  loadAndShow: function () {
     $('#options')
-      .on('show.bs.collapse', () => $('.options-button').addClass('options-button--expanded'))
-      .on('hide.bs.collapse', () => $('.options-button').removeClass('options-button--expanded'));
+      .on('show.bs.collapse', () =>
+        $('.options-button').addClass('options-button--expanded')
+      )
+      .on('hide.bs.collapse', () =>
+        $('.options-button').removeClass('options-button--expanded')
+      );
 
     try {
       const savedString = localStorage.getItem('options');
-      const saved = JSON.parse(savedString)
-        || { sameSize: true, samePushDate: true, maxRecords: 100 };
+      const saved = JSON.parse(savedString) || {
+        sameSize: true,
+        samePushDate: true,
+        maxRecords: 100,
+      };
 
       $('#sameSize').attr('checked', saved.sameSize);
       $('#samePushDate').attr('checked', saved.samePushDate);
@@ -497,7 +503,7 @@ const Options = {
     } catch {}
   },
 
-  getAndSave: function() {
+  getAndSave: function () {
     const sameSize = $('#sameSize').is(':checked');
     const samePushDate = $('#samePushDate').is(':checked');
     const maxRecords = $('#maxRecords').val();
@@ -507,7 +513,7 @@ const Options = {
       localStorage.setItem('options', JSON.stringify(val));
     } catch {}
     return val;
-  }
+  },
 };
 
 function SimilarChecker(options) {
@@ -539,7 +545,7 @@ function SimilarChecker(options) {
     if (key.length > 0) {
       similarForks.set(key, {
         diff_from_original: fork.diff_from_original,
-        diff_to_original: fork.diff_to_original
+        diff_to_original: fork.diff_to_original,
       });
     }
   }
